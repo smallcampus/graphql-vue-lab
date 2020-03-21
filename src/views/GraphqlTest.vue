1e3b92d3131714fd0ba035f78fc40c8378441499
Tin Mak <tin.ht.mak@gtomato.com>
2020-03-21 17:03:10 +0800

    Add graphql test

---
+++ src/views/GraphqlTest.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ projects }}</p>
+  </div>
+</template>
+<script>
+import gql from "graphql-tag";
+
+export default {
+  apollo: {
+    projects: gql`
+      query {
+        projects {
+          _id
+          name
+        }
+      }
+    `
+  }
+};
+</script>
