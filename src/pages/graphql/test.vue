700f768ed4f4c028dffcdc885066770909598ede
Tin Mak <tin.ht.mak@gtomato.com>
2020-03-21 20:59:58 +0800

    Add vue apollo test page

---
+++ src/pages/graphql/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ projects }}</h1>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  name: "graphql-test",
+  apollo: {
+    projects: gql`
+      query {
+        projects {
+          _id
+          name
+        }
+      }
+    `
+  }
+};
+</script>
--- src/vue-apollo.ts
+++ src/vue-apollo.ts
@@ -14,7 +14,7 @@ const AUTH_TOKEN = "apollo-token";
 
 // Http endpoint
 const httpEndpoint =
-  process.env.VUE_APP_GRAPHQL_HTTP || "http://localhost:4000/graphql";
+  process.env.VUE_APP_GRAPHQL_HTTP || "http://localhost:3300/graphql";
 
 // Config
 const defaultOptions = {
@@ -22,7 +22,7 @@ const defaultOptions = {
   httpEndpoint,
   // You can use `wss` for secure connection (recommended in production)
   // Use `null` to disable subscriptions
-  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || "ws://localhost:4000/graphql",
+  wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || "ws://localhost:3300/graphql",
   // LocalStorage token
   tokenName: AUTH_TOKEN,
   // Enable Automatic Query persisting with Apollo Engine
